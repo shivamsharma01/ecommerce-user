02d7f5c38e0272f900d10a7ef2b4728b95a38526
Shivam Sharma <shivamsharma.ade@gmail.com>
2026-03-08 12:32:23 +0530

    update object mapper bean to handle time conversions

--- src/main/java/com/mcart/user/utils/JwtConfig.java
+++ src/main/java/com/mcart/user/utils/JwtConfig.java
@@ -1,5 +1,7 @@
 package com.mcart.user.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -39,6 +41,13 @@ public class JwtConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 
 }
